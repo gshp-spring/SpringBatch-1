8dde9cf64e6f9e1aa3664bf012492770fc542e5c
Leandro Leao <lleao@dominio.nat>
2018-10-11 13:17:17 -0300

    segundo commit

---
+++ src/main/java/ar/com/nat/bat/UserItemProcessor.java
@@ -0,0 +1,20 @@
+package ar.com.nat.bat;
+
+import org.slf4j.Logger;
+import ar.com.nat.bat.entitys.User;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+public class UserItemProcessor implements ItemProcessor<User, User> {
+	   private static final Logger log = LoggerFactory.getLogger(UserItemProcessor.class);
+
+	   @Override
+	   public User process(final User user) throws Exception {
+	      final String firstName = user.getFirstName().toUpperCase();
+	      final String lastName = user.getLastName().toUpperCase();
+	      final User transformedPerson = new User(firstName, lastName);
+
+	      log.info("Converting (" + user + ") into (" + transformedPerson + ")");
+	      return transformedPerson;
+	   }
+	}
